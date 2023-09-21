600bedade5adaaac1977b20efb0627395f3e7a48
fullchico <diibarbosa22@gmail.com>
2023-09-21 09:43:02 -0300

    feat: config snapshot test styled

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,6 +4,7 @@ export const Main: React.FC = () => {
   return (
     <S.Wrapper>
       <h1>Ola Main</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</h2>
     </S.Wrapper>
   )
 }
